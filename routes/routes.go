23444926f2e767bdc0004187b6b93c4b4a01ab66
KrisnaSinatria <ketutkrisna626@gmail.com>
2026-07-30 17:45:52 +0800

    feat: create CRUD rest api

--- routes/routes.go
+++ routes/routes.go
@@ -1,11 +1,15 @@
 package routes
 
 import (
-    "go-first/controllers"
-    "github.com/gin-gonic/gin"
+	"go-first/controllers"
+
+	"github.com/gin-gonic/gin"
 )
 
 func SetupRoutes(router *gin.Engine) {
-    router.GET("/categories", controllers.GetCategories)
-    router.POST("/categories", controllers.CreateCategory)
+	router.GET("/categories", controllers.GetCategories)
+	router.POST("/categories", controllers.CreateCategory)
+	router.GET("/categories/:id", controllers.GetCategoryByID)
+	router.PUT("/categories/:id", controllers.UpdateCategory)
+	router.DELETE("/categories/:id", controllers.DeleteCategory)
 }
